0f6e18555f67849fa95e4b561f549826960a82d0
Pablo Gila Herranz <pablo.gila.herranz@gmail.com>
2023-12-04 22:43:37 +0100

    Fully working with PDB files

--- input-maker.py
+++ input-maker.py
@@ -1,60 +1,71 @@
 import os
 import glob
+from ase.io import read
 
 
-####################################################
-# This script copies the cell parameters from the CP2K input files
-####################################################
-# Template and final input files:
+############################################################
+## Create CP2K input files from a template, easy piecy.
+############################################################
+##  Template and final input files.
+##  The cell goes after this keyword in the template:
+template_cell_keyword = '!REPLACE_CELL_HERE' 
 template = 'CP2K_MAPI_template.inp'
-template_cell_keyword = '!REPLACE_CELL_HERE' # The cell goes after this string in the template
 final_file = 'CP2K_MAPI.inp'
-# For already-existing INP input files, please rename the extension
-cell_format = '.inp_' # Whatever, just rename the INP extension
-cell_keyword = '&CELL'
-# Or for CELL files: STILL NOT SUPPORTED
-# cell_format = '.cell'
-# cell_keyword = '%BLOCK LATTICE_CART'
-# TO-DO: USE ASE TO READ PDB AND GET THE CELL PARAMETERS
-####################################################
+##  For PDB, CELL or ASE-compatible files:
+cell_format = '.pdb'
+##  For already-existing INP input files, rename to .inp_ or
+##  similar to avoid problems, mantaining the 'inp' string:
+#cell_format = '.inp_'
+############################################################
 
 
 def cell_from_inp(inp_file_path):
 
     with open(inp_file_path, 'r') as inp_file:
         inp_content = inp_file.readlines()
-    a_row = b_row = c_row = None
 
+    cell_rows = [None, None, None]
     for i, line in enumerate(inp_content):
-        if line.strip() == cell_keyword:
-            a_row = inp_content[i+1].strip() if inp_content[i+1].strip().startswith('A ') else None
-            a_row = a_row[2:]
-            b_row = inp_content[i+2].strip() if inp_content[i+2].strip().startswith('B ') else None
-            b_row = b_row[2:]
-            c_row = inp_content[i+3].strip() if inp_content[i+3].strip().startswith('C ') else None
-            c_row = c_row[2:]
-
-        if all([a_row, b_row, c_row]):
-            return a_row, b_row, c_row
+        if line.strip() == '&CELL':
+            for j in range(3):
+                row = inp_content[i+j+1].strip()
+                if row.startswith(('A ', 'B ', 'C ')):
+                    cell_rows[j] = row[2:]
+            return tuple(cell_rows) 
 
 
-def write_cell(template, new_file, a_row, b_row, c_row):
+def cell_from_ase(structure_file_path):
+    try:
+        structure = read(structure_file_path)
+        cell = structure.cell[:]
+        transposed_cell = zip(*cell)
+        cell_rows = ["     ".join(f"{num:.15f}" for num in row) for row in transposed_cell]
+        return tuple(cell_rows)
+    except:
+        print("ERROR: ASE can't read " + structure_file_path + ", skipping...")
+        return None
+
+
+def write_cell(template, new_file, rows):
+
+    if not all(rows) and not len(rows) == 3:
+        error_file = os.path.dirname(new_file)
+        print("ERROR: Didn't find the cell parameters in " + error_file)
+        return
 
     with open(template, 'r') as template_file:
         template_content = template_file.readlines()
 
     for i, line in enumerate(template_content):
         if line.strip() == template_cell_keyword:
-            template_content[i+1] = "        A " + a_row + "\n"
-            template_content[i+2] = "        B " + b_row + "\n"
-            template_content[i+3] = "        C " + c_row + "\n"
+            template_content[i+1] = "        A   " + rows[0] + "\n"
+            template_content[i+2] = "        B   " + rows[1] + "\n"
+            template_content[i+3] = "        C   " + rows[2] + "\n"
 
             print("Writting input in " + new_file)
             with open(new_file, 'w') as new_file:
                 new_file.writelines(template_content)
-
             return
-    
     print("ERROR: Didn't find the '" + template_cell_keyword + "' keyword")
     print("       in the '" + template + "' template.")
 
@@ -65,21 +76,21 @@ def main():
 
         if os.path.isdir(folder):
             new_file = os.path.join(folder, final_file)
-            inp_file_path = glob.glob(os.path.join(folder, '*' + cell_format))
+            structure_file_path = glob.glob(os.path.join(folder, '*' + cell_format))
 
-            if not inp_file_path: # Skip empty folders
+            if not structure_file_path: # Skip empty folders
                 continue
-            if len(inp_file_path) > 1:
+            if len(structure_file_path) > 1:
                 print("ERROR: More than one input found in " + folder + ", skipping...")
                 continue
-            inp_file_path = inp_file_path[0]
+            structure_file_path = structure_file_path[0]
 
-            a_row, b_row, c_row = cell_from_inp(inp_file_path)
-            if not all([a_row, b_row, c_row]):
-                print("ERROR: Didn't find the cell parameters in " + inp_file_path)
-                continue
+            if 'inp' in cell_format:
+                rows = cell_from_inp(structure_file_path)
+            else:
+                rows = cell_from_ase(structure_file_path)
 
-            write_cell(template, new_file, a_row, b_row, c_row)
+            write_cell(template, new_file, rows)
 
 
 if __name__ == "__main__":
