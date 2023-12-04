296c3107b4ca83a3d1c92e1925cf27dceb3b590f
Pablo Gila Herranz <pablo.gila.herranz@gmail.com>
2023-12-04 19:16:43 +0100

    IT WORKS

--- input-maker.py
+++ input-maker.py
@@ -1,11 +1,13 @@
 import os
 import glob
 
+
 ####################################################
 # This script copies the cell parameters from the CP2K input files
 ####################################################
 # Template and final input files:
 template = 'CP2K_MAPI_template.inp'
+template_cell_keyword = '!REPLACE_CELL_HERE' # The cell goes after this string in the template
 final_file = 'CP2K_MAPI.inp'
 # For already-existing INP input files, please rename the extension
 cell_format = '.inp_' # Whatever, just rename the INP extension
@@ -17,53 +19,69 @@ cell_keyword = '&CELL'
 ####################################################
 
 
-# Open the template file and read its content
-with open(template, 'r') as template_file:
-    template_content = template_file.readlines()
+def cell_from_inp(inp_file_path):
 
-# For each subfolder
-for folder in os.listdir('.'):
-    if os.path.isdir(folder):
-        inp_file_path = glob.glob(os.path.join(folder, '*' + cell_format))
-        if not inp_file_path:
-            continue
-        inp_file_path = inp_file_path[0]
+    with open(inp_file_path, 'r') as inp_file:
+        inp_content = inp_file.readlines()
+    a_row = b_row = c_row = None
 
-        with open(inp_file_path, 'r') as inp_file:
-            inp_content = inp_file.readlines()
+    for i, line in enumerate(inp_content):
+        if line.strip() == cell_keyword:
+            a_row = inp_content[i+1].strip() if inp_content[i+1].strip().startswith('A ') else None
+            a_row = a_row[2:]
+            b_row = inp_content[i+2].strip() if inp_content[i+2].strip().startswith('B ') else None
+            b_row = b_row[2:]
+            c_row = inp_content[i+3].strip() if inp_content[i+3].strip().startswith('C ') else None
+            c_row = c_row[2:]
 
-        # Extract the A, B, and C rows only if they are preceded by $CELL
-        a_row = b_row = c_row = None
-        for i, line in enumerate(inp_content):
-            if line.strip() == cell_keyword:
-                a_row = inp_content[i+1].strip() if inp_content[i+1].strip().startswith('A ') else None
-                b_row = inp_content[i+2].strip() if inp_content[i+2].strip().startswith('B ') else None
-                c_row = inp_content[i+3].strip() if inp_content[i+3].strip().startswith('C ') else None
-            if all([a_row, b_row, c_row]):
-                break  # Stop looking for CELL
+        if all([a_row, b_row, c_row]):
+            return a_row, b_row, c_row
 
-        if not all([a_row, b_row, c_row]):
-            print("ERROR: Didn't find the cell parameters in ", folder)
-            continue  # Skip this file if A, B, or C row is missing
 
-        # Replace the A, B, and C rows in the template content
-        a_done = b_done = c_done = False
-        for i, line in enumerate(template_content):
-            if line.strip() == '&CELL':
-                if template_content[i+1].strip().startswith('A '):
-                    template_content[i+1] = "        " + a_row + "\n"
-                    a_done = True
-                if template_content[i+2].strip().startswith('B '):
-                    template_content[i+2] = "        " + b_row + "\n"
-                    b_done = True
-                if template_content[i+3].strip().startswith('C '):
-                    template_content[i+3] = "        " + c_row + "\n"
-                    c_done = True
-            if all([a_done, b_done, c_done]):
-                print("Copied cell in ", folder)
-                break  # Stop looking for CELL
+def write_cell(template, new_file, a_row, b_row, c_row):
 
-        # Write the modified content to a new file in the subfolder
-        with open(os.path.join(folder, final_file), 'w') as new_file:
-            new_file.writelines(template_content)
+    with open(template, 'r') as template_file:
+        template_content = template_file.readlines()
+
+    for i, line in enumerate(template_content):
+        if line.strip() == template_cell_keyword:
+            template_content[i+1] = "        A " + a_row + "\n"
+            template_content[i+2] = "        B " + b_row + "\n"
+            template_content[i+3] = "        C " + c_row + "\n"
+
+            print("Writting input in " + new_file)
+            with open(new_file, 'w') as new_file:
+                new_file.writelines(template_content)
+
+            return
+    
+    print("ERROR: Didn't find the '" + template_cell_keyword + "' keyword")
+    print("       in the '" + template + "' template.")
+
+
+def main():
+    # For each subfolder
+    for folder in os.listdir('.'):
+
+        if os.path.isdir(folder):
+            new_file = os.path.join(folder, final_file)
+            inp_file_path = glob.glob(os.path.join(folder, '*' + cell_format))
+
+            if not inp_file_path: # Skip empty folders
+                continue
+            if len(inp_file_path) > 1:
+                print("ERROR: More than one input found in " + folder + ", skipping...")
+                continue
+            inp_file_path = inp_file_path[0]
+
+            a_row, b_row, c_row = cell_from_inp(inp_file_path)
+            if not all([a_row, b_row, c_row]):
+                print("ERROR: Didn't find the cell parameters in " + inp_file_path)
+                continue
+
+            write_cell(template, new_file, a_row, b_row, c_row)
+
+
+if __name__ == "__main__":
+    main()
 
